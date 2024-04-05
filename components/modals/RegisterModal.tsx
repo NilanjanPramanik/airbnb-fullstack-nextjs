a2e7bd434af884af8e67137a5e6b9d9bd8aa16d9
Nilanjan Pramanik <nik.nilanjan@gmail.com>
2024-04-06 01:52:50 +0530

    oAuth removed

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -101,20 +101,6 @@ const RegisterModal = () => {
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
-      {/* <Button
-        outline
-        label="Continue with Google"
-        icon={FcGoogle}
-        onClick={() => signIn('google',{
-          callbackUrl: process.env.NEXT_PUBLIC_APP_URL
-        })}
-      />
-      <Button
-        outline
-        label="Continue with Github"
-        icon={AiFillGithub}
-        onClick={() => signIn('github')}
-      /> */}
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="flex flex-row justify-center gap-2 items-center">
           <div>
